974d6fdb45ff2f3c63020b1ea2a355367360bc10
insilicomab <54536339+insilicomab@users.noreply.github.com>
2021-09-09 00:59:46 +0900

    add script to tests.py

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
-# Create your tests here.
+class TopPageTest(TestCase):
+    def test_top_page_returns_200_and_expected_title(self):
+        response = self.client.get("/")
+        self.assertContains(response, 'わんこの学びブログ', status_code=200)
+
+    def test_top_page_uses_expected_template(self):
+        response = self.client.get("/")
+        self.assertTemplateUsed(response, "blog/top.html")
